f230d0be6f81849c0395883da8bddb9944dfa811
Smit Vadhel <vadhelsmit@gmail.com>
2024-10-02 14:39:38 +0530

    Lec 7 - Why string to number conversion is confusing done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -32,3 +32,46 @@ let someNumber = 22
 
 
 // ******************** OPERATIONS ********************
+
+let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(3%2);
+
+let str1 = "Hello"
+let str2 = " Smit"
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2; // Should avoid using such tricks
+
+let a = 100
+let b = a++;
+console.log(`a:${a}, b:${b}`);
+
+let x = 100
+let y = ++x;
+console.log(`x:${x}, y:${y}`);
+
+// link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+

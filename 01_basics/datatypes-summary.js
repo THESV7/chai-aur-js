9af9a5d4f8292cb1a6051b88040e79159b389fe8
Smit Vadhel <vadhelsmit@gmail.com>
2024-10-05 17:35:59 +0530

    Lec 10 - Stack and Heap memory in javascript done

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,11 +12,11 @@ let userEmail;
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
-console.log(id, anotherId);
-console.log(id === anotherId);
+// console.log(id, anotherId);
+// console.log(id === anotherId);
 
 const bigNumber = 62735372n
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 // Reference (Non primitive)
 
@@ -32,3 +32,24 @@ console.log(typeof bigNumber); // You can check type of all datatypes here
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-primitive)
+
+let myName = "Smit"
+let anotherName = myName
+anotherName = "Amit"
+console.log(myName);
+console.log(anotherName);
+
+
+let userOne = {
+    email: "abc@google.com",
+    upi: "user@axis"
+}
+let userTwo = userOne
+userTwo.email = "smit@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
